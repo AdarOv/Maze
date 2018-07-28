048051bbcfe3cd7c5a6136f2aa0e96ae17bef8d3
adarOv <adarov@post.bgu.ac.il>
2018-07-28 13:36:00 +0300

    create repository

---
+++ Model/IModel.cs
@@ -0,0 +1,28 @@
+﻿using ATP2016Project.Model.Algorithms.MazeGenerators;
+using ATP2016Project.Model.Algorithms.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATP2016Project.Model
+{
+    /// <summary>
+    /// An Interface represents a model
+    /// </summary>
+    interface IModel
+    {
+        string[] getDir(string path);
+        void generate3dMaze(string mazeName, int columns, int rows, int levels);
+        int[,,] getMaze(string mazeName);        
+        bool saveMaze(string mazeName, string path);
+        void loadMaze(string path, string mazeName);
+        int mazeSize(string mazeName);
+        long fileSize(string path);
+        void solveMaze(string mazeName, string algorithm);
+        Solution displaySolution(string mazeName);
+        bool mazeExists(string mazeName);
+        void exit();
+    }
+}
